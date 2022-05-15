37d4326d472a82ba0b530bae7af06a084d09689b
Akshath Kaushal <akshath.kaushal@gmail.com>
2022-05-15 14:14:16 +0530

    github actions

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -6,4 +6,4 @@ echo "This is the secret: $SSH_PASSWORD"
 echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
 echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 
-ansible-playbook ansible/playbook.yml
+ansible-playbook ansible/playbook.yml --user akshath
